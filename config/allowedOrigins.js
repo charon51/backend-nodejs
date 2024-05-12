7a091d502d6e07190448fea2847040736de78b26
charon51 <138808497+charon51@users.noreply.github.com>
2024-05-12 10:24:01 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
+    "https://d2u7lhc48m3f06.cloudfront.net",
     "https://d33fea3juik5mr.cloudfront.net",
     "https://d2rbl2zzrcnh7t.cloudfront.net",
-    "https://charon511.vercel.app",
     "http://localhost:3001"
     
 ];
