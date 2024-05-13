97111eb6d11b11119ff498f3f7223a0158572918
charon51 <138808497+charon51@users.noreply.github.com>
2024-05-13 12:33:15 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+    "https://d6h738os3irtf.cloudfront.net",
     "https://d2s0b583z30qq1.cloudfront.net",
     "https://d2u7lhc48m3f06.cloudfront.net",
     "https://d2u7lhc48m3f06.cloudfront.net",
