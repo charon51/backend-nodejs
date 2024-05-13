a8cc38f02b82a9830b5497895950bf0ce0e66406
charon51 <138808497+charon51@users.noreply.github.com>
2024-05-13 12:57:05 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     "https://d2s0b583z30qq1.cloudfront.net",
     "https://d2u7lhc48m3f06.cloudfront.net",
     "https://d35mo1wma7b0sh.cloudfront.net",
+    "https://frontend-topaz-delta.vercel.app",
     "http://localhost:3000",
     "http://localhost:3001"
     
