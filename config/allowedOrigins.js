72ec3e34b15a94ab91df5e1916e7b34e2ad2b4ea
charon51 <138808497+charon51@users.noreply.github.com>
2024-05-12 21:07:45 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     "https://d2u7lhc48m3f06.cloudfront.net",
     "https://d33fea3juik5mr.cloudfront.net",
     "https://d2rbl2zzrcnh7t.cloudfront.net",
+    "http://localhost:3000",
     "http://localhost:3001"
     
 ];
